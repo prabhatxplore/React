15da937489f3ffc805033f4ee8c6cc4e32ecf6c7
Prabhat Bk <prabhatbk4@gmail.com>
2026-02-20 22:48:52 +0545

    configured the fetch

--- airbnb-client/src/context/AuthContext.jsx
+++ airbnb-client/src/context/AuthContext.jsx
@@ -1,26 +1,29 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        fetch("http://localhost:3000/api/session-user", {
-            credentials: "include"
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setUser(data.user)
-            })// user exist only set otherwise set null
-    }, [])
+  useEffect(() => {
+    console.log("hello");
+    fetch("/api/session-user", {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data) {
+          setUser(data.user);
+        }
+      })
+      .catch((err) => console.log("Error Fetching", err)); // user exist only set otherwise set null
+  }, []);
 
-    return (
-        <AuthContext.Provider value={{ user, setUser }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-export const useAuth = () => useContext(AuthContext)
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
--- airbnb-client/src/pages/Home.jsx
+++ airbnb-client/src/pages/Home.jsx
@@ -1,41 +1,47 @@
 import { useEffect, useState } from "react";
-import Card from "../components/card";
 import { useAuth } from "../context/AuthContext";
-
-
+import Card from "../components/Card";
 
 export default function Home() {
-    const { setUser } = useAuth();
-    const [homes, setHomes] = useState([]);
-    useEffect(() => {
+  const { setUser } = useAuth();
+  const [homes, setHomes] = useState([]);
+  useEffect(() => {
+    fetch("/api/home", {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data.user);
+        setHomes(data.homes);
+        console.log(data);
+      })
+      .catch((err) => console.log("Error Occured while fetching homes", err));
+  }, []);
 
-        fetch("http://localhost:3000/api/home", {
-            credentials: "include"
-        }).then(res => res.json()).then((data) => {
-            setUser(data.user);
-            setHomes(data.homes)
-            console.log(data)
-        })
-    }, [])
+  return (
+    <>
+      <div className="w-[50vw] mx-auto gap-5 py-14  rounded-3xl h-fit items-center flex flex-col">
+        <h1 className="text-center text-[#FF385C] text-4xl font-bold">
+          Welcome to Airbnb
+        </h1>
 
-    return (
-        <>
-            <div className="w-[50vw] mx-auto gap-5 py-14  rounded-3xl h-fit items-center flex flex-col">
-                <h1 className="text-center text-[#FF385C] text-4xl font-bold">Welcome to Airbnb</h1>
-
-                <a href="/host/add-home"
-                    className="bg-[#FF385C] hover:scale-105 transition duration-150 rounded-[8px] p-1 px-3 font-medium text-white">
-                    Add Home
-                </a>
-            </div>
-            <div className="border-t-[1px] border-t-gray-300 px-3 overflow-y-auto">
-                <h1 className=" text-center p-6 rounded-3xl font-medium text-2xl ">
-                    Available Homes
-                </h1>
-                <div className="flex gap-9">
-                    {homes.map(home => <Card key={home._id} home={home} />)}
-                </div>
-            </div>
-        </>
-    )
+        <a
+          href="/host/add-home"
+          className="bg-[#FF385C] hover:scale-105 transition duration-150 rounded-[8px] p-1 px-3 font-medium text-white"
+        >
+          Add Home
+        </a>
+      </div>
+      <div className="border-t-[1px] border-t-gray-300 px-3 overflow-y-auto">
+        <h1 className=" text-center p-6 rounded-3xl font-medium text-2xl ">
+          Available Homes
+        </h1>
+        <div className="flex gap-9">
+          {homes.map((home) => (
+            <Card key={home._id} home={home} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
