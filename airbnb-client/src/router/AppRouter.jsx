55a654971dbce43941bd4df299a2e972aa3d059b
Prabhat Bk <prabhatbk4@gmail.com>
2026-02-21 21:07:04 +0545

    routed new pages

--- airbnb-client/src/router/AppRouter.jsx
+++ airbnb-client/src/router/AppRouter.jsx
@@ -1,34 +1,49 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Home from '../pages/Home'
-import Error from '../pages/Error'
-import App from '../App'
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
-import HomeDetails from '../pages/HomeDetails'
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Error from "../pages/Error";
+import App from "../App";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import HomeDetails from "../pages/HomeDetails";
+import AddHome from "../pages/AddHome";
+import HostHomes from "../pages/HostHomes";
+import EditHome from "../pages/EditHome";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        errorElement: <Error />,
-        children: [
-            {
-                index: true,
-                element: <Home />
-            },
-            {
-                path: "login",
-                element: <Login />
-            },
-            {
-                path: "signup",
-                element: <Signup />
-            },
-            {
-                path: "home-details/:homeId",
-                element: <HomeDetails />,
-            }
-        ]
-    },
-])
-export default router
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+      {
+        path: "home-details/:homeID",
+        element: <HomeDetails />,
+      },
+      {
+        path: "/add-home",
+        element: <AddHome />,
+      },
+      {
+        path: "/host-homes",
+        element: <HostHomes />,
+      },
+      {
+        path: "/edit-home/:editHomeID",
+        element: <EditHome />,
+      },
+    ],
+  },
+]);
+export default router;
