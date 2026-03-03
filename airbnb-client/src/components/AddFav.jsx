1452d2b6bf0cca85ab84b9f068ef1fbc52c13dee
Prabhat BK <prabhatbk4@gmail.com>
2026-03-04 01:12:06 +0545

    fixed the like issue

--- airbnb-client/src/components/AddFav.jsx
+++ airbnb-client/src/components/AddFav.jsx
@@ -19,6 +19,7 @@ function AddFav({ _id }) {
     }
 
     useEffect(() => {
+        if (!_id) return
         if (favourites?.length > 0) {
             const favId = favourites.map(favHome => favHome._id.toString())
             if (favId.includes(_id?.toString())) {
@@ -29,26 +30,26 @@ function AddFav({ _id }) {
         } else {
             setFav(false)
         }
-    }, [favourites])
+    }, [favourites, _id])
     return (
-            <button onClick={handleSubmitFav}
-                className="p-2 bg-white bg-opacity-75 hover:bg-opacity-100 rounded-full cursor-pointer shadow-md transition-colors flex items-center justify-center"
+        <button onClick={handleSubmitFav}
+            className="p-2 bg-white bg-opacity-75 hover:bg-opacity-100 rounded-full cursor-pointer shadow-md transition-colors flex items-center justify-center"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill={fav ? "#FF385C" : "none"}
+                stroke={fav ? "#FF385C" : "currentColor"}
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                className="w-5 h-5"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill={fav ? "#FF385C" : "none"}
-                    stroke={fav ? "#FF385C" : "currentColor"}
-                    viewBox="0 0 24 24"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                    />
-                </svg>
-            </button>
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                />
+            </svg>
+        </button>
     )
 }
 
