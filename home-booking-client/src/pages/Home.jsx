36e0ffb02484e4fcc4337921a34f21bb96ea2724
Prabhat BK <prabhatbk4@gmail.com>
2026-03-09 20:40:16 +0545

    deleted no use

--- home-booking-client/src/pages/Home.jsx
+++ home-booking-client/src/pages/Home.jsx
@@ -22,17 +22,9 @@ export default function Home() {
 
   return (
     <>
-      {user?.usertype == "host" && <div className="w-[50vw] mx-auto gap-5 py-14  rounded-3xl h-fit items-center flex flex-col">
-        <h1 className="text-center text-[#FF385C] text-4xl font-bold">
-          Welcome to Airbnb
-        </h1>
-        <Link
-          to="/add-home"
-          className="bg-[#FF385C] hover:scale-105 transition duration-150 rounded-[8px] p-1 px-3 font-medium text-white"
-        >
-          Add Home
-        </Link>
-      </div>}
+      <div>
+        
+      </div>
 
       <div className="border-t-[1px] border-t-gray-300 px-3 overflow-y-auto">
         <h1 className=" text-center p-6 rounded-3xl font-medium text-2xl ">
